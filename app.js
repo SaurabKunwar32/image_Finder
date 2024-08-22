fb06364f5509c112216b536634d20083301388ff
Saurab Kunwar <saurabkunwar188@gmail.com>
2024-08-22 23:59:01 +0545

    updates

--- app.js
+++ app.js
@@ -9,52 +9,48 @@ const showMore = document.getElementById("show_more");
 let inpData = ""; //we can store all the input data  entered by the user
 let page = 1; //by default the page number is 1
 
-
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    page = 1;
-    imageSearch();
-  });
-  
-  showMore.addEventListener("click", () => {
-    imageSearch();
-  });
+  event.preventDefault();
+  page = 1;
+  imageSearch();
+});
 
-  
+showMore.addEventListener("click", () => {
+  imageSearch();
+});
+
 async function imageSearch() {
   // we are using the async to fetch data
   inpData = input.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inpData}&client_id=${accessKey}` ; //we create our own dynamic url to based on our input data
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inpData}&client_id=${accessKey}`; //we create our own dynamic url to based on our input data
 
   const response = await fetch(url);
   const data = await response.json(); //data stores all the  json format data
 
-  const receiveResults = data.results;  
+  const receiveResults = data.results;
 
   if (page === 1) {
-    S_result.innerHTML = " ";  //we do this to remove the the images that we have added before to fix the size of the images after searching.
+    S_result.innerHTML = " "; //we do this to remove the the images that we have added before to fix the size of the images after searching.
   }
-
-
 
   receiveResults.map((result) => {
     // we should connect all the link to the container that we have cerated in html
-    
-    const imgCon = document.createElement('div');
+
+    const imgCon = document.createElement("div");
     imgCon.classList.add("search_resultt");
-    const image = document.createElement('img');
+    const image = document.createElement("img");
     image.src = result.urls.small;
     image.alt = result.alt_description;
-    const imgLink = document.createElement('a');
+    const imgLink = document.createElement("a");
     imgLink.href = result.links.html;
     imgLink.target = "_blank";
     imgLink.textContent = result.alt_description;
-        Search_res.style.display="block";
+    Search_res.style.display = "block";
 
     imgCon.appendChild(image);
     imgCon.appendChild(imgLink);
     S_result.appendChild(imgCon);
-    S_result.style.visibility="visible";
+    S_result.style.visibility = "visible";
   });
 
   page++;
@@ -62,4 +58,3 @@ async function imageSearch() {
     showMore.style.display = "block";
   }
 }
-
